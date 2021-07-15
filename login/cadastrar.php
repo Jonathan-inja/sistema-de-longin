b1b9e672dd2173631260ef0b8d5c7a06c056bd4d
Jonathan Silva <jonathan.inja6@gmail.com>
2021-07-15 14:17:40 -0300

    Telas de login e cadastro prontas

---
+++ login/cadastrar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sistema de Cadastro</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+
+        <div id="corpo-form-cad">
+        
+            <h2>Cadastrar</h2>
+          
+            <form action="">
+                <input type="text" placeholder="Nome Completo">
+                <input type="text" placeholder="Nome de Usuario">
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Senha">
+                <input type="password" placeholder="Confirmar Senha">
+                <input type="submit" value="Cadastrar">
+                <a href="login.php">Já sou cadastrado!<strong> Login.<strong</a>
+            </form>
+     
+        </div>
+    </body>
+
+</html>
